29dcf9ba734f323e878b31e1a367eaffec6dab44
nao-maro <ttc2550sg0006@edu.tech.ac.jp>
2025-07-03 11:59:48 +0900

    ねじこアニメーション
    動きのプログラムを追加しました

--- Candy_6_19/Assets/Scripts/nejikoController.cs
+++ Candy_6_19/Assets/Scripts/nejikoController.cs
@@ -3,24 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class nejikoController : MonoBehaviour
+
+   
 {
     // Start is called before the first frame update
-    
-    //1.プレイヤーのキー入力を受け取る
-    //2.キー入力の方向にあわせて移動する
-    //3.移動方向にあわせてアニメーションを再生する
-    
+
+
+
+    CharacterController controller;
+    Vector3 moveDirection = Vector3.zero;
+    public float speed = 0f;
+    Animator animator;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetAxis("Vertical") > 0.0f)
         {
-            Debug.Log("Test");
+            moveDirection.z = Input.GetAxis("Vertical") * speed;
         }
+        else
+        {
+            moveDirection.z = 0.0f;
+        }
+
+        //Horisontal(左右入力)があれば、ねじこを回転させる
+        transform.Rotate(0, Input.GetAxis("Horizontal") * 3f, 0);
+        
+        //Jumpキーの入力があればy軸に力をかける
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = 10f;
+            animator.SetTrigger("jump");
+        }
+        
+        //移動量をTransfomeに変換する
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+        //Controllerに移動量を渡す
+        controller.Move(globalDirection);
+
+        animator.SetBool("run", moveDirection.z > 0f);
     }
 }
